00878141b5174abca49a0e0854adbb3b05c1a137
Roman Reztsov <roman.reztsov@aira.cz>
2021-01-24 04:40:27 +0100

    final

--- MDAV_2_REZTSOV.R
+++ MDAV_2_REZTSOV.R
@@ -2,10 +2,11 @@ library(dplyr)
 library(ggplot2)
 library(openxlsx)
 
-
+# definice cesty k cvs souboram
 getwd()
 setwd('/home/pinguin/Documents/Unicorn2020/Analyza dat - semestralka1/Analyza_dat_2/data')
 
+# načítání souborů
 oct <- read.table('2019-Oct.csv', sep=',', dec='.', na.strings=c(" ", ""), header = T, encoding = "UTF-8") 
 nov <- read.table('2019-Nov.csv', sep=',', dec='.', na.strings=c(" ", ""), header = T, encoding = "UTF-8") 
 dec <- read.table('2019-Dec.csv', sep=',', dec='.', na.strings=c(" ", ""), header = T, encoding = "UTF-8") 
@@ -13,11 +14,11 @@ jan <- read.table('2020-Jan.csv', sep=',', dec='.', na.strings=c(" ", ""), heade
 feb <- read.table('2020-Feb.csv', sep=',', dec='.', na.strings=c(" ", ""), header = T, encoding = "UTF-8") 
 
 
-
+# spojení vstupních dat do jedneho data.fame. Dělám to abych se dalo zpracovávat podle týdnů. Jinak musel bych resitspojeni týdnů na hranice měsíců. 
 data <- rbind(oct, nov, dec, jan, feb)
 
+# mazání dat, které už se nebudou používat. Pro šetření operacky
 rm(oct,nov,dec,jan,feb)
-
 
 
 na_delete <- function(df, cols){
@@ -39,129 +40,109 @@ priprava_dat <- function(df){
   return(df)
 }
 
-count_mounth_statistics <- function(df){
-  objednane_produkty = df %>% filter(event_type == 'purchase')
+count_mounth_statistics <- function(df){ # funkce zpracovává 1 měsíc
+  objednane_produkty = df %>% filter(event_type == 'purchase') # filtrování jen objednané zboží
   
-  prijem <- sum(objednane_produkty$price)
+  prijem <- sum(objednane_produkty$price) # příjem
   
-  objednavky <- objednane_produkty %>% group_by(user_session) %>% summarize(sum = sum(price), .groups='drop') 
-  prumer_objednavky <- mean(objednavky$sum) 
-  objednavky_count <- nrow(objednavky)
-  rm(objednavky)
-  visits <- df %>% group_by(user_session) %>% summarize(user_id = first(user_id), .groups='drop') %>% nrow()
-  konverze_visit_buy <- objednavky_count*100/visits
+  objednavky <- objednane_produkty %>% group_by(user_session) %>% summarize(sum = sum(price), .groups='drop') # tím získám objednávky. Jeden uživatel může objednat více zboží za jeden krát. Abych to identifikovat, udělám group_by session_id
+
+  prumer_objednavky <- mean(objednavky$sum) # průměrná hodnota objednávky 
+  objednavky_count <- nrow(objednavky) # počet objednávek
+  rm(objednavky) # mazání dat, které už se nebudou používat. Pro šetření operacky
+  visits <- df %>% group_by(user_session) %>% summarize(user_id = first(user_id), .groups='drop') %>% nrow() # tím získám počet návštěvníků. Grupuju podle seesion_id abych vyhodit duplicity
+  konverze_visit_buy <- objednavky_count*100/visits # konverze
   
-  #produkty <- rle(sort(objednane_produkty$product_id))
-  #prodej_produktu <- data.frame(prod_id=produkty$values, n_buy=produkty$lengths)
-  #rm(produkty)
+  brand_prodej <- objednane_produkty %>% group_by(brand) %>% summarise(brand = first(brand), prijem = sum(price), .groups='drop') # prodeje podle brandů
   
-  #zobrazene_produkty <- df %>% filter(event_type == 'view')
-  #produkty <- rle(sort(zobrazene_produkty$product_id))
-  #zobrazeni_produktu <- data.frame(prod_id=produkty$values, n_show=produkty$lengths)
+  kategorie_prodej <- objednane_produkty %>% group_by(category_id) %>% summarise(category_id = first(category_id), prijem = sum(price), .groups='drop') # prodeje podle kategorie
   
-  
-  #konverze_produktu <- left_join(zobrazeni_produktu, prodej_produktu, by='prod_id')
-  
-  #konverze_produktu[is.na(konverze_produktu)] <- 0
-  #konverze_produktu$konverze <-konverze_produktu$n_buy*100/konverze_produktu$n_show
-  brand_prodej <- objednane_produkty %>% group_by(brand) %>% summarise(brand = first(brand), prijem = sum(price), .groups='drop')
-  
-  kategorie_prodej <- objednane_produkty %>% group_by(category_id) %>% summarise(category_id = first(category_id), prijem = sum(price), .groups='drop')
-  
-  results <- list(prijem = prijem, prumer_objednavky = prumer_objednavky, objednavky_count = objednavky_count, konverze_visit_buy = konverze_visit_buy, brand_prodej = brand_prodej, kategorie_prodej = kategorie_prodej)
+  # ukládám výsledky do listu
+  results <- list(prijem = prijem, prumer_objednavky = prumer_objednavky, objednavky_count = objednavky_count, konverze_visit_buy = konverze_visit_buy, brand_prodej = brand_prodej, kategorie_prodej = kategorie_prodej) 
   return(results)
 }
 
-count_week_statistics <- function(df){
-  objednane_produkty = df %>% filter(event_type == 'purchase')
+count_week_statistics <- function(df){ #funkce zpracovává 1 tyden
+  objednane_produkty = df %>% filter(event_type == 'purchase') # filtrování jen objednané zboží
 
-  prijem <- sum(objednane_produkty$price)
+  prijem <- sum(objednane_produkty$price) # příjem
   
-  objednavky <- objednane_produkty %>% group_by(user_session) %>% summarize(sum = sum(price), .groups='drop') 
-  prumer_objednavky <- mean(objednavky$sum) 
-  objednavky_count <- nrow(objednavky)
-  rm(objednavky)
-  visits <- df %>% group_by(user_session) %>% summarize(user_id = first(user_id), .groups='drop') %>% nrow()
-  konverze_visit_buy <- objednavky_count*100/visits
+  objednavky <- objednane_produkty %>% group_by(user_session) %>% summarize(sum = sum(price), .groups='drop') # tím získám objednávky. Jeden uživatel může objednat více zboží za jeden krát. Abych to identifikovat, udělám group_by session_id
+  prumer_objednavky <- mean(objednavky$sum) # průměrná hodnota objednávky 
+  objednavky_count <- nrow(objednavky)# počet objednávek
+  rm(objednavky) # mazání dat, které už se nebudou používat. Pro šetření operacky
+  visits <- df %>% group_by(user_session) %>% summarize(user_id = first(user_id), .groups='drop') %>% nrow() # tím získám počet návštěvníků. Grupuju podle seesion_id abych vyhodit duplicity
+  konverze_visit_buy <- objednavky_count*100/visits # konverze
   results <- list(prijem = prijem, prumer_objednavky = prumer_objednavky, objednavky_count = objednavky_count, konverze_visit_buy = konverze_visit_buy)
+  # ukládám výsledky do listu
   return(results)
 }
 
-count_day_statistics <- function(df){
+count_day_statistics <- function(df){ #funkce zpracovává 1 den
   objednane_produkty = df %>% filter(event_type == 'purchase')
   
-  prijem <- sum(objednane_produkty$price)
+  prijem <- sum(objednane_produkty$price) # příjem
   
-  #objednavky <- objednane_produkty %>% group_by(user_session) %>% summarize(sum = sum(price), .groups='drop') 
-  #prumer_objednavky <- mean(objednavky$sum) 
-  #objednavky_count <- nrow(objednavky)
-  #results <- list(prijem = prijem, prumer_objednavky = prumer_objednavky, objednavky_count = objednavky_count)
+  # ukládám výsledky do listu
   results <- list(prijem = prijem)
-  
   return(results)
 }
 
-count_statistics <- function(data, month=T, week=T, day=T){
-  final_result <- list()
-  if(month){
-    final_result[['month_prijmy']] <- data.frame(month=c('1','2','3','4','5'), prijem=c(NA, NA, NA, NA, NA))
-    final_result[['month_objednavka_mean']] <- data.frame(month=c('1','2','3','4','5'), prumer=c(NA, NA, NA, NA, NA))
-    final_result[['month_objednavky_count']] <- data.frame(month=c('1','2','3','4','5'), count=c(NA, NA, NA, NA, NA))
-    final_result[['month_konverze_visit_buy']] <- data.frame(month=c('1','2','3','4','5'), value=c(NA, NA, NA, NA, NA))
-    #final_result[['month_sales_products']] <- data.frame(month=c('1','2','3','4','5'), min_count=c(NA, NA, NA, NA, NA), max_count=c(NA, NA, NA, NA, NA))
-    #final_result[['month_popular_products']] <- data.frame(month=c('1','2','3','4','5'), min_count=c(NA, NA, NA, NA, NA), max_count=c(NA, NA, NA, NA, NA))
+count_statistics <- function(data, month=T, week=T, day=T){ # hlavní funkce, která postupně prochází měsíce, týdne a dny a ukládá výsledky výpočtů
+  final_result <- list() # definice proměny z vysledkama
+  if(month){ 
+    final_result[['month_prijmy']] <- data.frame(month=c('1','2','3','4','5'), prijem=c(NA, NA, NA, NA, NA)) # zakládání data.frame pro příjem
+    final_result[['month_objednavka_mean']] <- data.frame(month=c('1','2','3','4','5'), prumer=c(NA, NA, NA, NA, NA)) #zakládání data.frame pro průměrnou hodnotu objednávky
+    final_result[['month_objednavky_count']] <- data.frame(month=c('1','2','3','4','5'), count=c(NA, NA, NA, NA, NA)) #zakládání data.frame pro počet objednávek
+    final_result[['month_konverze_visit_buy']] <- data.frame(month=c('1','2','3','4','5'), value=c(NA, NA, NA, NA, NA)) #zakládání data.frame pro konverze
     
-    brands <-unique(data$brand)
-    brands[is.na(brands)] <- 'No brand'
+    brands <-unique(data$brand) # získání seznamu brandů
+    brands[is.na(brands)] <- 'No brand' # přidání No brand
     
-    final_result[['month_brand_sales']] <- data.frame(brand=brands)
-    rm(brands)
+    final_result[['month_brand_sales']] <- data.frame(brand=brands) # zakládání data.frame pro brandy
+    rm(brands) # mazání dat, které už se nebudou používat. Pro šetření operacky
     
     categories <-unique(data$category_id)
     categories[is.na(categories)] <- 'No category'
     
     final_result[['month_category_sales']] <- data.frame(category_id=categories)
-    rm(categories)
+    rm(categories) # mazání dat, které už se nebudou používat. Pro šetření operacky
     
     
     
-    mounth_statistics <- list()
-    for(a in 1:5){
-      month <- data %>% filter(month == a) %>% count_mounth_statistics()
-      list_name = paste0('month_', a)
-      mounth_statistics[[list_name]] <- month
-      rm(month)
+    mounth_statistics <- list() # definice proměně pro měsíční statistiky
+    for(a in 1:5){ # proházím cyklusem 5 měsíců
+      month <- data %>% filter(month == a) %>% count_mounth_statistics() # výběr dat za 1 měsíc a spuštění funkce count_mounth_statistics pro ten měsíc
+      list_name = paste0('month_', a) # definice názvu listu v výsledkách 
+      mounth_statistics[[list_name]] <- month # ukládání výsledků
+      rm(month) # mazání dat, které už se nebudou používat. Pro šetření operacky
     }
     
-    for(page in 1:5){
+    for(page in 1:5){ # ukládání výsledků pro všechny měsíce do jedneho data.frame
       final_result[['month_prijmy']][page, 2] <-mounth_statistics[[page]]$prijem
       final_result[['month_objednavka_mean']][page, 2] <- mounth_statistics[[page]]$prumer_objednavky
       final_result[['month_objednavky_count']][page, 2] <- mounth_statistics[[page]]$objednavky_count
       final_result[['month_konverze_visit_buy']][page, 2] <- mounth_statistics[[page]]$konverze_visit_buy
-      #kp <- mounth_statistics[[page]]$konverze_produktu
-      #final_result[['month_sales_products']][page, 2] <- filter(kp, n_buy < 1)
-      #final_result[['month_sales_products']][page, 3] <- filter(kp, n_buy > 10)
-      #final_result[['month_popular_products']][page, 2] <- filter(kp, n_show < 10)
-      #final_result[['month_popular_products']][page, 3] <- filter(kp, n_show > 850)
-      #rm(kp)
       
-      bp <- mounth_statistics[[page]]$brand_prodej
+      # výpočet podílu prodeje brandu na celkovim
+      bp <- mounth_statistics[[page]]$brand_prodej 
       bp$brand[is.na(bp$brand)] <- 'No brand'
       bp$prijem <- bp$prijem*100/mounth_statistics[[page]]$prijem
       colnames(bp) <- c('brand', paste0('prijem_', page))
       final_result[['month_brand_sales']] <-right_join(final_result[['month_brand_sales']], bp, by='brand')
-      rm(bp)
+      rm(bp) # mazání dat, které už se nebudou používat. Pro šetření operacky
       
+      # výpočet podílu prodeje kategorie na celkovim
       catp <- mounth_statistics[[page]]$kategorie_prodej
       catp$category_id[is.na(catp$category_id)] <- 'No category'
       catp$prijem <- catp$prijem*100/mounth_statistics[[page]]$prijem
       colnames(catp) <- c('category_id', paste0('prijem_', page))
-      #catp$category_id <- as.character(catp$category_id)
-      #  print(head(catp))
+      
       final_result[['month_category_sales']] <-right_join(final_result[['month_category_sales']], catp, by='category_id')
-      rm(catp)
+      rm(catp) # mazání dat, které už se nebudou používat. Pro šetření operacky
     }
     
+    # výpočet indexu změny prodeje podle minulého měsíce
     final_result[['month_brand_sales']]$index2_to_1 <- final_result[['month_brand_sales']]$prijem_2/final_result[['month_brand_sales']]$prijem_1*100-100
     final_result[['month_brand_sales']]$index3_to_2 <- final_result[['month_brand_sales']]$prijem_3/final_result[['month_brand_sales']]$prijem_2*100-100
     final_result[['month_brand_sales']]$index4_to_3 <- final_result[['month_brand_sales']]$prijem_4/final_result[['month_brand_sales']]$prijem_3*100-100
@@ -169,33 +150,32 @@ count_statistics <- function(data, month=T, week=T, day=T){
     final_result[['month_brand_sales']]$prumer <- rowMeans(final_result[['month_brand_sales']][,c('prijem_1','prijem_2','prijem_3','prijem_4','prijem_5')])
     final_result[['month_brand_sales']][is.na(final_result[['month_brand_sales']] )] <- 0 
     
+    # výpočet indexu změny prodeje podle minulého měsíce
     final_result[['month_category_sales']]$index2_to_1 <- final_result[['month_category_sales']]$prijem_2/final_result[['month_category_sales']]$prijem_1*100-100
     final_result[['month_category_sales']]$index3_to_2 <- final_result[['month_category_sales']]$prijem_3/final_result[['month_category_sales']]$prijem_2*100-100
     final_result[['month_category_sales']]$index4_to_3 <- final_result[['month_category_sales']]$prijem_4/final_result[['month_category_sales']]$prijem_3*100-100
     final_result[['month_category_sales']]$index5_to_4 <- final_result[['month_category_sales']]$prijem_5/final_result[['month_category_sales']]$prijem_4*100-100
-    
     final_result[['month_category_sales']]$prumer <- rowMeans(final_result[['month_category_sales']][,c('prijem_1','prijem_2','prijem_3','prijem_4','prijem_5')])
-    
     final_result[['month_category_sales']][is.na(final_result[['month_category_sales']] )] <- 0 
     
   }
   
   if(week){
-    week_statistics <- list()
-    for(a in 1:22){
-      week <- data %>% filter(week == a) %>% count_week_statistics()
-      list_name = paste0('week_', a)
-      week_statistics[[list_name]] <- week
+    week_statistics <- list() # definice proměně pro týdenní statistiky
+    for(a in 1:22){ # proházím cyklusem 22 týdnů
+      week <- data %>% filter(week == a) %>% count_week_statistics() # výběr dat za 1 týden a spuštění funkce count_week_statistics
+      list_name = paste0('week_', a) # definice názvu listu v výsledkách 
+      week_statistics[[list_name]] <- week # ukládání výsledků
     }
     
-    #final_result <- list()
-    final_result[['week_prijmy']] <- data.frame(week=seq(1,22), prijem=rep(NA, 22))
+    # zakládání data.frame stejně jako pro měsíce
+    final_result[['week_prijmy']] <- data.frame(week=seq(1,22), prijem=rep(NA, 22)) 
     final_result[['week_objednavka_mean']] <- data.frame(week=seq(1,22), prumer=rep(NA, 22))
     final_result[['week_objednavky_count']] <- data.frame(week=seq(1,22), count=rep(NA, 22))
     final_result[['week_konverze_visit_buy']] <- data.frame(week=seq(1,22), value=rep(NA, 22))
     
     
-    for(page in 1:22){
+    for(page in 1:22){ # ukládání výsledků pro všechny týdny do jedneho data.frame
       final_result[['week_prijmy']][page, 2] <-week_statistics[[page]]$prijem
       final_result[['week_objednavka_mean']][page, 2] <- week_statistics[[page]]$prumer_objednavky
       final_result[['week_objednavky_count']][page, 2] <- week_statistics[[page]]$objednavky_count
@@ -205,18 +185,18 @@ count_statistics <- function(data, month=T, week=T, day=T){
   }
   
   if(day){
-    day_statistics <- list()
-    for(a in 1:152){
-      day <- data %>% filter(day == a) %>% count_day_statistics()
-      list_name = paste0('day', a)
-      day_statistics[[list_name]] <- day
+    day_statistics <- list() # definice proměně pro denní statistiky
+    for(a in 1:152){ # proházím cyklusem 152 dnů
+      day <- data %>% filter(day == a) %>% count_day_statistics() # výběr dat za 1 den a spuštění funkce count_day_statistics
+      list_name = paste0('day', a) # definice názvu listu v výsledkách 
+      day_statistics[[list_name]] <- day # ukládání výsledků
     }
     
     #final_result <- list()
-    final_result[['day_prijmy']] <- data.frame(day=seq(1,152), prijem=rep(NA, 152))
+    final_result[['day_prijmy']] <- data.frame(day=seq(1,152), prijem=rep(NA, 152)) # zakládání data.frame pro příjem
     
     
-    for(page in 1:152){
+    for(page in 1:152){ # ukládání výsledků pro všechny dny do jedneho data.frame
       final_result[['day_prijmy']][page, 2] <-day_statistics[[page]]$prijem
     }
   }
@@ -227,7 +207,7 @@ count_statistics <- function(data, month=T, week=T, day=T){
 
 
 
-
+# zpuštění výpočtu
 data <- na_delete(data, colnames(data)[-c(5,6)])
 data <- priprava_dat(data)
 
@@ -235,9 +215,7 @@ data <- priprava_dat(data)
 result <- count_statistics(data, T, T, T)
 
 
-
-
-
+# kreslení grafů
 ggplot(data=result[['month_prijmy']], aes(x=month, y=prijem, group = 1))+geom_line()+geom_line(aes(y=mean(prijem)))+geom_point()+labs(x='Měsíce', y='Příjmy', title="Graf příjmů podle měsíce")
 ggplot(data=result[['month_objednavka_mean']], aes(x=month, y=prumer, group = 1))+geom_line()+geom_line(aes(y=mean(prumer))) + geom_point()+labs(x='Měsíce', y='Průměrná hodnota', title="Průměr objednávky podle měsíce")
 ggplot(data=result[['month_objednavky_count']], aes(x=month, y=count, group = 1))+geom_line()+geom_line(aes(y=mean(count)))  + geom_point()+labs(x='Měsíce', y='Počet', title="Počet objednávek podle měsíce")
@@ -260,7 +238,7 @@ ggplot(data=result[['week_konverze_visit_buy']], aes(x=week, y=value, group = 1)
 
 ggplot(data=result[['day_prijmy']], aes(x=day, y=prijem, group = 1))+geom_line()+geom_line(aes(y=mean(prijem)))+geom_point()+labs(x='Dny', y='Příjmy', title="Graf příjmů podle dnů")
   
-
+# generování tabulek pro přílohy
 write.xlsx(result[['month_brand_sales']], 'month_brand_sales.xlsx')
 write.xlsx(result[['month_category_sales']], 'month_category_sales.xlsx')
 
